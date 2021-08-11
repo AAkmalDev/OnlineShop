45027b2819c992dcd48e7f4d1890354e60e81d7c
AAkmalDev <developerakmal@gmail.com>
2021-08-11 15:11:36 +0500

    first commit

---
+++ app/src/main/java/uz/koinot/onlineshop/MainActivity.kt
@@ -0,0 +1,17 @@
+package uz.koinot.onlineshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
+
+
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
